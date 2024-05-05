b73fee913cb36a01a3523f2572080d74d7338181
sohilaehab25 <sohailaehab25@gmail.com>
2024-05-05 19:32:30 +0200

    make sure that email is uniqe

--- controller/userController.js
+++ controller/userController.js
@@ -5,26 +5,28 @@ var randomToken = require("random-token");
 
 
 exports.insertuser = (req, res, next) => {
-    // const errors = validationResult(req);
-    // if (!errors.isEmpty()) {
-    //     return res.status(400).json({ errors: errors.array() });
-    // }
     const {username, email, password, status, call_back } = req.body;
     console.log(req.body);
-
-    const newUser = new userschema({
-        username:username,
-        email: email,
-        password: password,
-        status: status,
-        call_back: call_back
-    });
-
-    newUser.save()
-      .then((data) =>{
-        res.status(200).json({data});
-      })
-      .catch((error => next(error)));
+    userschema.findOne({email}).exec() 
+        .then(existingUser => {
+            if(existingUser){
+                return res.status(400).json({error:"This email is already in use. Please enter another email."})
+            }
+            
+            const newUser = new userschema({
+                username: username,
+                email: email,
+                password: password,
+                status: status,
+                call_back: call_back
+            });
+        
+            return newUser.save();
+        })
+        .then(data => {
+            res.status(200).json({data, message:"Registration successful."});
+        })
+        .catch(error => next(error));
 };
 
 exports.getAllUSers = (req,res,next) =>{
@@ -38,7 +40,7 @@ exports.getUserId = (req,res,next) => {
     userschema.findOne({_id: req.params._id}) 
   .then((data) => {
         if (!data) {
-            return res.status(404).json({ message: 'child not found' });
+            return res.status(404).json({ message: 'user not found' });
         }
         res.status(200).json(data);
     }).catch((error) => { next(error) });
